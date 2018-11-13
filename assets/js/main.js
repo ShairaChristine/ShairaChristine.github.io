39cd86f08507bb643a84c4570682e674bdb7be55
shairachristine <shaics03@gmail.com>
2018-11-13 20:55:20 +0800

    finally almost finish

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+  	slideIndex = slides.length
+  }    
+  if (n < 1) {
+  	slideIndex = 1
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  $(slides[slideIndex]).hide( "slide", { direction: "left"  }, 1500 );
+  $(slides[slideIndex-1]).show( "slide", {direction: "right" }, 1500 );
+  slides[slideIndex-1].style.display = "block";
+
+  dots[slideIndex-1].className += " active";
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
