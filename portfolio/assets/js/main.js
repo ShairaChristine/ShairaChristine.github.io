831129ad6cbe0a35947ba860eb41719014c05021
unknown <shaics03@gmail.com>
2019-03-28 01:34:20 +0800

    done UI for portfolio

---
+++ portfolio/assets/js/main.js
@@ -0,0 +1,1 @@
+jQuery(function($){$(document).ready(function(){for_css();});});function for_css(){var cssHTML=' <link rel="stylesheet" type="text/css" href="assets/css/main.css">'+'<link rel="stylesheet" type="text/css" href="assets/css/responsive.css">';$('head').append(cssHTML);}
